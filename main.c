92c0b4b730bc25676827238f3c9a0aff958c22f7
H.A.L <52590821+connectome55512@users.noreply.github.com>
2022-12-06 14:51:32 -0500

    Update main.c

--- main.c
+++ main.c
@@ -37,9 +37,6 @@
 //adds a new name for some existing data type but does not create a new type 
 //typedef <existing_name> <alias_name>
 
-
-
-
 // we have declared an struct type variable Date with the typedef keyword. Now we can create struct type variables with Date 
 typedef struct 
 {
@@ -70,6 +67,9 @@ typedef struct
 }s_EmployeesInfo;
 
 
+
+
+======================
 
 void printMessageCenter(const char* message)
 {
@@ -117,7 +117,8 @@ void welcomeMessage()
     printf("\n\n\n\t\t\t Enter any key to continue.....");
     getch();
 }
-//Validate name
+
+//----------IS_NAME_VALID
 int isNameValid(const char *name)
 {
     int validName = 1;
@@ -134,8 +135,11 @@ int isNameValid(const char *name)
     }
     return validName;
 }
+
+
 // Function to check leap year.
 //Function returns 1 if leap year
+//------------IS_LEAP_YEAR
 int  IsLeapYear(int year)
 {
     return (((year % 4 == 0) &&
@@ -143,6 +147,9 @@ int  IsLeapYear(int year)
             (year % 400 == 0));
 }
 // returns 1 if given date is valid.
+
+
+//-------------IS_VALID_DATE 
 int isValidDate(Date *validDate)
 {
     //check range of year,month and day
@@ -167,77 +174,155 @@ int isValidDate(Date *validDate)
         return (validDate->dd <= 30);
     return 1;
 }
-// Add employee in list
-void addEmployeeInDataBase()
+
+//----------IS_FILE_EXISTS
+
+//Check file exist or not
+//path is pointer to a chr 
+int isFileExists(const char *path)
 {
-    // size of addEmployeeInfoDataBase is equal to size of s_employeeInfo struct (6 data types)
-    s_EmployeesInfo addEmployeeInfoInDataBase = {0};
-    FILE *fp = NULL;
+    // Try to open file
+    FILE *fp = fopen(path, "rb");
     int status = 0;
-    fp = fopen(FILE_NAME,"ab+");
+    // If file does not exists
+    if (fp != NULL)
+    {
+        status = 1;
+        // File exists hence close file
+        fclose(fp);
+    }
+    return status;
+}
+
+
+
+//----------init()
+
+
+
+//----------LOGIN()
+
+
+
+
+//----------MAIN()
+
+
+
+//----------MENU()
+
+
+
+
+//----------UPDATE_CREDENTIAL 
+
+
+
+//----------ADD EMPLOYEE
+
+
+
+
+//-----------VIEW EMPLOYEE DETAILS
+
+
+
+//-----------SEARCH EMPLOYEE
+
+
+
+//-----------DELETE EMPLOYEE 
+void deleteEmployee()
+{
+    //initialize found variable = false, employee id is not located yet 
+    //changed
+    int exists = 0;
+    //intialize variable for employee ID, the program will read in an integer value that the user enters 
+    //change deleteID
+    int employeeDelete = 0;
+    //struct sFileData = fileDataInfo
+    sFileHeader fileHeaderInfo = {0};
+
+    s_EmployeesInfo addEmployeeInfoInDataBase = {0};
+    //declare pointers for both files, reg and temporary
+    FILE *fp = NULL;
+    FILE *tmpFp = NULL;
+    //display head message for this scenario of deleting an employee
+    //calls headMessage function and inputs message
+    headMessage("Delete employee Details");
+    //open file, read
+    fp = fopen(FILE_NAME,"rb");
+    //if file is NULL, it does not exist 
     if(fp == NULL)
     {
-        printf("File is not opened\n");
+        printf("Error: File does not exist\n");
         exit(1);
     }
-    headMessage("ADD NEW EMPLOYEES");
-    printf("\n\n\t\t\tENTER YOUR DETAILS BELOW:");
-    printf("\n\t\t\t---------------------------------------------------------------------------\n");
-    printf("\n\t\t\tEmployee ID  = ");
-    fflush(stdin);
-    scanf("%u",&addEmployeeInfoInDataBase.employee_id);
-    do
+    //open temporary file, write to 
+    tmpFp = fopen("tmp.bin","wb");
+    //if file is NULL, does not exist 
+    if(tmpFp == NULL)
     {
-        printf("\n\t\t\tFather Name  = ");
-        fflush(stdin);
-        fgets(addEmployeeInfoInDataBase.LastName,MAX_LAST_NAME,stdin);
-        status = isNameValid(addEmployeeInfoInDataBase.LastName);
-        if (!status)
+        fclose(fp);
+        printf("Error: File does not exist\n");
+        exit(1);
+    }
+    //fread reads data from a file and stores it in a buffer
+    //fread takes in parameters 
+    //Buffer: Pointer to the buffer where data will be stored. A buffer is a region of memory used to temporarily store data
+    //Size: The size of each element to be read in bytes
+    //Count: Number of elements to be read
+    //Stream: Pointer to the FILE object from where data is to be read
+    fread (&fileHeaderInfo,FILE_HEADER_SIZE, 1, fp);
+
+    //fwrite() function writes binary and text data from an array to a given data stream.
+    //buffer: points to the first object in the array to be written
+    //size: the item size in bytes
+    //count: the number of the objects to be written
+    //stream: points to a FILE object that specifies an output stream
+
+    fwrite(&fileHeaderInfo, FILE_HEADER_SIZE, 1, tmpFp);
+  
+    printf("\n\t\t\tEnter employee ID NO. for delete:");
+    // the program will read in an integer value that the user enters
+    //When the user enters an integer, it is stored in the employeeDelete variable.
+    // &employeeDelete gets the address of employeeDelete, and the value entered by the user is stored in that address.
+    scanf("%d",&employeeDelete);
+   
+
+
+    // fread reads data from a file and stores it in a buffer
+    // fread takes in parameters 
+    //Buffer: Pointer to the buffer where data will be stored. A buffer is a region of memory used to temporarily store data
+    //Size: The size of each element to be read in bytes
+    //Count: Number of elements to be read
+    //Stream: Pointer to the FILE object from where data is to be read
+
+    while (fread (&addEmployeeInfoInDataBase, sizeof(addEmployeeInfoInDataBase), 1, fp))
+    {
+        //if employeeDelete does not match an employee ID from the database
+        if(addEmployeeInfoInDataBase.employee_id != employeeDelete)
         {
-            printf("\n\t\t\tName contain invalid character. Please enter again.");
+            fwrite(&addEmployeeInfoInDataBase,sizeof(addEmployeeInfoInDataBase), 1, tmpFp);
+        }
+        else
+        //employee ID does already exist, exist variable = 1 or true
+        {
+            exists = 1;
         }
     }
-    while(!status);
-    do
-    {
-        printf("\n\t\t\tEmployee Name  = ");
-        fflush(stdin);
-        fgets(addEmployeeInfoInDataBase.FirstName,MAX_FIRST_NAME,stdin);
-        status = isNameValid(addEmployeeInfoInDataBase.FirstName);
-        if (!status)
-        {
-            printf("\n\t\t\tName contain invalid character. Please enter again.");
-        }
-    }
-    while(!status);
-    do
-    {
-        printf("\n\t\t\tEmployee Address  = ");
-        fflush(stdin);
-        fgets(addEmployeeInfoInDataBase.employeeAddr,MAX_LAST_NAME,stdin);
-        status = isNameValid(addEmployeeInfoInDataBase.employeeAddr);
-        if (!status)
-        {
-            printf("\n\t\t\tName contain invalid character. Please enter again.");
-        }
-    }
-    while(!status);
-    printf("\n\t\t\tEmployee Salary  = ");
-    fflush(stdin);
-    scanf("%f",&addEmployeeInfoInDataBase.employeeSalary);
-    do
-    {
-        //get date year,month and day from user
-        printf("\n\t\t\tEnter date in format (day/month/year): ");
-        scanf("%d/%d/%d",&addEmployeeInfoInDataBase.employeeJoiningDate.dd,&addEmployeeInfoInDataBase.employeeJoiningDate.mm,&addEmployeeInfoInDataBase.employeeJoiningDate.yyyy);
-        //check date validity
-        status = isValidDate(&addEmployeeInfoInDataBase.employeeJoiningDate);
-        if (!status)
-        {
-            printf("\n\t\t\tPlease enter a valid date.\n");
-        }
-    }
-    while(!status);
-    fwrite(&addEmployeeInfoInDataBase,sizeof(addEmployeeInfoInDataBase), 1, fp);
+    ////if exists is true then print Record deleted
+    //otherwise record not found 
+    //? operator, if condition is true then x(Record deleted successfully): otherwise y(Record not found)
+    (exists)? printf("\n\t\t\tRecord deleted successfully....."):printf("\n\t\t\tRecord not found");
+
+    //close all files 
     fclose(fp);
+    fclose(tmpFp);
+    //delete the file Employee_Records 
+    remove(FILE_NAME);
+    //rename the temporary file to Employee_Records to keep an updated record 
+    rename("tmp.bin",FILE_NAME);
 }
+
+
